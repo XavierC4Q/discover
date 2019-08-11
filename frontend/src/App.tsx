536564b9352f0266f57942f0aa2f6da4b040a13c
Xavier <munroexavier@gmail.com>
2019-08-11 19:59:24 -0400

    tslint rules update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import SearchAppBar from './components/SearchBar';
 import './styles/App.css';
 
 const App: React.FC = () => {
   return (
-    <Container maxWidth={false} className="App">
-      <Content />
-      <Footer />
-    </Container>
+    <React.Fragment>
+      <CssBaseline />
+      <SearchAppBar />
+      <Container maxWidth={false} className="App">
+        <Content />
+        <Footer />
+      </Container>
+    </React.Fragment>
   );
 };
 
